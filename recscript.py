afa9c25b80dd2b292437064dad5bb3dcedec1214
ChrisNguyen <cnguyen258@gmail.com>
2018-05-05 21:50:46 -0500

    Got deletion script working

--- recscript.py
+++ recscript.py
@@ -1,3 +1,13 @@
 """
+Take gb files s and e (start and end)
+Compare them to determine whether the mutation is an insertion, deletion, or substitution
+
 DELETION (easy)
+
+Find which bases have been deleted
+
+Calculate the length of the deletion (start - end)
+
 """
+
+print "Hello world!"
